b849fbb21bcdd045ae607eebc396a27c17078fef
Trinity Cisneros <trinitycisneros@Trinitys-MacBook-Pro.local>
2019-10-15 17:28:37 -0700

    make better'

--- static/js/navbase.js
+++ static/js/navbase.js
@@ -1,5 +1,10 @@
-$(document).ready(function () {
-    $("#navbar-frame").load("../templates/navbar.html");
-});
-
 console.log("js working");
+console.log("Hello world!");
+
+
+function load_navbase() {
+    document.getElementById("navbar-frame").innerHTML='<object type="text/html" data="navbar.html" ></object>';
+    console.log("in function");
+}
+
+load_navbase();
