f06b372e3c8647b4838f2dc46294fd96f5c791ab
Trinity Cisneros <trinitycisneros@Trinitys-MacBook-Pro.local>
2019-10-24 19:33:04 -0700

    updated themes and folder structure

---
+++ static/js/nav_multi.js
@@ -0,0 +1,12 @@
+$('.dropdown-submenu > a').on("click", function(e) {
+    var submenu = $(this);
+    $('.dropdown-submenu .dropdown-menu').removeClass('show');
+    submenu.next('.dropdown-menu').addClass('show');
+    e.stopPropagation();
+});
+
+$('.dropdown').on("hidden.bs.dropdown", function() {
+    // hide any open menus when parent closes
+    $('.dropdown-menu.show').removeClass('show');
+});
+
