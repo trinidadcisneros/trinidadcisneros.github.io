39e4a6230ce8f746fe311f6a57334130a5067bd1
Trinity Cisneros <trinitycisneros@Trinitys-MacBook-Pro.local>
2020-01-25 23:25:55 -0800

    updated python dictionary post

---
+++ folders/python/static/js/dict_table.js
@@ -0,0 +1,35 @@
+function contructTable(species) {
+  d3.csv(`static/documents/python_dictionary.csv`).then(function(data) {
+
+    // Select the table header tag
+    var thead = d3.select("thead");
+    thead.html("");
+
+    // Append info for each warning to the table body
+    const header_keys = Object.keys(data[0]);
+    Object.entries(header_keys).forEach(function([key, value]) {
+      //Append all values of the event to the row
+      var th = thead.append("th").text(value);
+    });
+
+    // Select the table body tag
+    var tbody = d3.select("tbody");
+    tbody.html("");
+
+    data.forEach(event => {
+      //Create new row for each warning
+      var row = tbody.append("tr");
+      Object.entries(event).forEach(function([key, value]) {
+        {
+          var td = row.append("td").text(value);
+        }
+      });
+    });
+  });
+}
+
+function init() {
+  contructTable();
+}
+
+init();
